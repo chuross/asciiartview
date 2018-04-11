9c74e632e8c40a758383b195ffc4000422b04c76
chuross <azarasi.mo.mo@gmail.com>
2018-04-12 03:50:26 +0900

    update sample aa

--- asciiartview/src/main/java/com/github/chuross/asciiartview/AsciiArtView.java
+++ asciiartview/src/main/java/com/github/chuross/asciiartview/AsciiArtView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -117,6 +118,11 @@ public class AsciiArtView extends View {
 
     public void setTypeface(Typeface typeface) {
         defaultAsciiArtPaint.setTypeface(typeface);
+        invalidate();
+    }
+
+    public void setTextColor(@ColorInt int color) {
+        defaultAsciiArtPaint.setColor(color);
         invalidate();
     }
 }
